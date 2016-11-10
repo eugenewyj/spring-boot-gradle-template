81d8664044e688a83791da2228ed5425ac9b5c17
Eugene Wang <fengbaoxp@gmail.com>
2016-11-10 11:11:01 +0800

    初始化项目

---
+++ src/main/java/org/eugene/DemoApplication.java
@@ -0,0 +1,22 @@
+package org.eugene;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+	/**
+	 * 主入口方法
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
